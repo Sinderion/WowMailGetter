fc78e95f9103f10c21f0e2f83e62a7cc9a11e80b
Sinderion <jwinsatt@yahoo.com>
2018-05-08 12:42:45 -0400

    Debugging

--- WowMailGetter.lua
+++ WowMailGetter.lua
@@ -3,6 +3,63 @@
 -- =========
 
 ZLM = LibStub("AceAddon-3.0"):NewAddon("ZatenkeinsLotteryManager", "AceConsole-3.0", "AceEvent-3.0");
+ZLM_OptionsTable = {
+    type = "group",
+    chidlGroups = "tab",
+    args = {
+        config = {
+            name = "Config",
+            type="group",
+            args = {
+                enable = {
+                    name = "Enable",
+                    desc = "Enables/disables the addon",
+                    type = "toggle",
+                    set = "SetEnabled",
+                    get = "GetEnabled"
+                },
+                debug = {
+                    name = "Debug Print Level",
+                    desc = "Displays addon output messages.  Lower values show only urgent messages.  Higher values show all messages.",
+                    type = "range",
+                    min = 0,
+                    max = 4,
+                    step = 1,
+                    bigStep = 1,
+                    set = "SetPrintLevel",
+                    get = "GetPrintLevel"
+                },
+                lotteryMethod = {
+                    name = "Lottery Method",
+                    desc = "The method of determining the winners.  Raffle assigns a proportional chance of winning based on total points.  Competition determines winner exclusively by point values.",
+                    type = "select",
+                    set = "SetLotteryMethod",
+                    get = "GetLotteryMethod",
+                    values = ZLM_LotteryMethod
+                },
+                numberOfWinners = {
+                    name = "Number of Winners",
+                    desc = "The number of winners you wish to get.",
+                    type = "range",
+                    min = 1,
+                    max = 100,
+                    step = 1,
+                    bigStep = 1,
+                    set = "SetWinnerCount",
+                    get = "GetWinnerCount"
+                },
+                exclusiveWinners = {
+                    name = "Exclusive Winners",
+                    desc = "[Raffle Method Only]Can the same person win more than once per drawing?",
+                    type = "toggle",
+                    set = "SetExclusiveWinners",
+                    get = "GetExclusiveWinners"
+                },
+            }
+        }
+        
+    }
+}
 
 function ZLM:OnInitialize()
     self.db = LibStub("AceDB-3.0"):New("ZatenkeinsLotteryManagerDB", defaults, true)
@@ -63,8 +120,10 @@ ZLM.db = ZLM.db or {};
 ZLM.db.profile = ZLM.db.profile or {};
 ZLM.db.profile.PrintLevel = ZLM.db.profile.PrintLevel or 2;
 function ZLM:Debug(message,severity)
-    if self.db.profile.PrintLevel > severity then
-        self.Print(message);
+	severity = severity or 0;
+	message = message or "Undefined debug message. Check your call.";
+    if 5 > severity then
+        print(message);
     end
 	-- ZLM:Debug(": " .. string.format("%s",value),1);
 end
@@ -82,7 +141,10 @@ ZLM.Mail.MailDelay = 0.1; -- Timer delay for taking mail. May need tuning.
 ZLM.Mail.NoDelay = 0.001; -- Timer delay for general multi-threaded work.
 ZLM.Mail.Snapshots = {};
 ZLM.Mail.Snapshots.Bags = {};
-function ZLM.Mail:Fullname(name)
+ZLM_TOTALATTEMPTS = 0;
+ZLM_TOTALSUCCESSES = 0;
+ZLM_TOTALRETRIES = 0;
+function ZLM.Mail:FullName(name)
 -- Sinderion -> Sinderion-ShadowCouncil (add server name to same-server sender);   Xaionics-BlackwaterRaiders -> Xaionics-BlackwaterRaiders (no change if it's already full)
 	if type(name) ~= "string" then
 		return nil;
@@ -93,6 +155,7 @@ function ZLM.Mail:Fullname(name)
 	else
 		name = name .. "-" .. GetRealmName(); -- Name needs some TLC. Might accidentally get NPC's with one name. Oh well lol.
 	end
+	ZLM:Debug(name, 2);
 	return name;
 end
 function ZLM.Mail:NoBagSpace()
@@ -103,18 +166,18 @@ function ZLM.Mail:NoBagSpace()
 	return s < 4;
 end
 function ZLM.Mail:BagsSnapshot()
-	Snapshot = {};
-	for i= 1-5 do
-		numberOfSlots = GetContainerNumSlots(bagID);
-		for for j = 1-numberOfSlots do
+	local Snapshot = {};
+	for i= 1, 5 do
+		numberOfSlots = GetContainerNumSlots(i);
+		for j = 1, numberOfSlots do
 			itemId = GetContainerItemID(i, j);
 			if itemId then
 				Snapshot[itemId] = Snapshot[itemId] or 0;
-				Snapshot[itemId] = snapshot[itemId] + select(2, GetContainerItemInfo(i,j))); -- select the 2nd return value (itemcount)
+				Snapshot[itemId] = Snapshot[itemId] + select(2, GetContainerItemInfo(i,j)); -- select the 2nd return value (itemcount)
 			end
 		end
 	end
-	return Snapshot  
+	return Snapshot;
 end
 function ZLM.Mail:FindValidInboxItem()
 -- Blindly finds the first attachment available and sent from someone without a space in their name(probably a player).
@@ -129,6 +192,7 @@ textCreated, canReply, isGM = GetInboxHeaderInfo(i);
 			for j = 1, 12 do
 				local name, itemID, texture, count, quality, canUse  = GetInboxItem(i, j);
 				if itemID then
+					print(sender .. name);
 					return { sender, count, itemID, i, j };
 				end
 			end
@@ -143,12 +207,13 @@ function ZLM.Mail:PitchMail(Index,itemIndex)
 end
 function ZLM.Mail:VerifiedTakeInboxItem(status)
 	Status = status or "INIT"
-	if status == "INIT" or status == "RETRY" then  	-- INIT and RETRY mean try and take the next item. RETRY, obviously, means try the same thing again.
+	ZLM.Mail.Current = ZLM.Mail.Current or {};
 		ZLM.Mail.Snapshots.Bags.Before = ZLM.Mail:BagsSnapshot();
 		if ZLM.Mail:NoBagSpace() then  
 			ZLM.Mail.Current.ItemID = nil; -- Full bags, no item to grab.
 			print("Bags full!"); -- Maybe change, or add a normal red middle of screen frame warning using standard error method.
 		elseif status == "RETRY" then -- just use the current tracking variables,ZLM.Mail.Current.ItemID, etc.
+			ZLM_TOTALRETRIES = ZLM_TOTALRETRIES + 1;
 		else -- Set tracking variables to the next bit of mail. 
 			local sender, count, itemID, Index, itemIndex = ZLM.Mail:FindValidInboxItem();
 			if sender then
@@ -159,15 +224,20 @@ function ZLM.Mail:VerifiedTakeInboxItem(status)
 				Index = Index,
 				ItemIndex = itemIndex
 				}
+				print(ZLM.Mail.Current.ItemID);
+			else
+			    ZLM:Debug("Can't find anymore mail.", 1);
 			end
 		end -- nil if no mail left, or no bag space. Otherwise there's SOMETHING to check, so check.
 		if not not ZLM.Mail.Current.ItemID then 
+		    ZLM_TOTALATTEMPTS = ZLM_TOTALATTEMPTS + 1;
 			ZLM.Mail:PitchMail(ZLM.Mail.Current.Index, ZLM.Mail.Current.ItemIndex);
 		else --end?
 			ZLM:UpdateScoreboard();
+			ZLM:Debug("Total: ".. ZLM_TOTALATTEMPTS .." Retries: " .. ZLM_TOTALRETRIES.. " Success: ".. ZLM_TOTALSUCCESSES, 1);
 			ZLM:Debug("itemID = nil for whatever reason. Exiting.", 1);
 		end
-	end
+
 end
 function ZLM.Mail:VerifyReciept()
 		ZLM:UnregisterEvent("BAG_UPDATE"); --
@@ -176,9 +246,10 @@ function ZLM.Mail:VerifyReciept()
 		if ZLM.Mail.Snapshots.After[ZLM.Mail.Current.ItemID] == ZLM.Mail.Snapshots.Bags.Before[ZLM.Mail.Current.ItemID] + ZLM.Mail.Current.Count then
 			ZLM:RecordDonation(ZLM.Mail:FullName(ZLM.Mail.Current.Sender),ZLM.Mail.Current.ItemID,ZLM.Mail.Current.Count);
 			ZLM.Mail.Current.ItemID = nil;
+			ZLM_TOTALSUCCESSES = ZLM_TOTALSUCCESSES + 1;
 			C_Timer.After(ZLM.Mail.NoDelay, function() ZLM.Mail:VerifiedTakeInboxItem("INIT") end);  
 		else
-			ZLM:Debug("Debug: Failed to get mail at ".. ZLM.Mail.Current.Index.." item " ZLM.Mail.Current.ItemIndex.." retrying...",0);
+			ZLM:Debug("Debug: Failed to get mail at ".. ZLM.Mail.Current.Index.." item " ..ZLM.Mail.Current.ItemIndex.." retrying...",0);
 			C_Timer.After(ZLM.Mail.MailDelay, function() ZLM.Mail:VerifiedTakeInboxItem("RETRY") end);
 		end
 end
